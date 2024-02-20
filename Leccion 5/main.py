b69e3352ef47bebb0dde6f55ee0341e2a271557d
Sebastian Navea <seba.n.a@outlook.com>
2024-02-20 16:47:00 -0300

    ciclo while descendente

--- Leccion 5/main.py
+++ Leccion 5/main.py
@@ -10,6 +10,7 @@
 #else:
 #    print(f'fin del ciclo while')
 
+#ciclo while imprimr numero de 0 a 5
 maximo = int(5)
 
 contador = int(0)
@@ -20,4 +21,14 @@ while contador <= maximo:
 else:
     print(f'fin de ciclo')
 
+#Ciclo while descendente imprimir numero de 5 a 1
+
+minimo = int (1)
+contador = int (5)
+
+while contador >= minimo:
+    print(contador)
+    contador -=1
+else:
+    print(f'Fin del ciclo')
 
