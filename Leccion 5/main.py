0bd96dfd85b1a4c791e4aa6fd5b5278dd71f7540
Sebastian Navea <seba.n.a@outlook.com>
2024-02-20 14:07:18 -0300

    primer ejercicio ciclo while

--- Leccion 5/main.py
+++ Leccion 5/main.py
@@ -1,1 +1,23 @@
-print ("Hello world")
+#print ("Hello world")
+
+#ejercicio while
+
+#contador = 0
+
+#while contador < 3:
+#    print(contador)
+#    contador += 1
+#else:
+#    print(f'fin del ciclo while')
+
+maximo = int(5)
+
+contador = int(0)
+
+while contador <= maximo:
+    print(contador)
+    contador +=1
+else:
+    print(f'fin de ciclo')
+
+
