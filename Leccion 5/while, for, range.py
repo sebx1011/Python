f8ee6db83d6deb0122cba3b1d24c3ffd66eeb3c9
Sebastian Navea <seba.n.a@outlook.com>
2024-02-21 15:50:08 -0300

    ciclos, listas y tuplas

--- Leccion 5/while, for, range.py
+++ Leccion 5/while, for, range.py
@@ -70,7 +70,8 @@
 #   print(f'valor: {i}')
 
 #crear unm rango de numeros de 2 a 6 e imprimirlos
-  
+#los 2 funcan pero de distinta manera  
+
 # for i in range(2,7):
 #   print(f'valor: {i}')
 
@@ -80,7 +81,8 @@
 #   print(i)
 
 # #crea un rango de 3 a 10, pero con incremento de 2 en 2, en lugar de 1 en 1
-  
+#primero nada que ver, no funca
+ 
 # for i in range(3, 10):
 #   if i < i+1:
 #     print (f'valor: {i}')
